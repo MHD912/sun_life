26b9341c791771b71b0a3f25f5df27655af166c5
Mohammad Hussein Hamed <hussein.912.workshop@gmail.com>
2023-12-24 23:17:28 +0300

    Initial commit

---
+++ android/app/src/main/java/com/example/sun_life/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.sun_life;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Hello World!'),
+      ),
+    );
+  }
+}
