84bf770fca1e4b91a16c1e4dc604d44e0c8ce48a
Mohammad Hussein Hamed <hussein.912.workshop@gmail.com>
2023-12-24 23:18:07 +0300

    Added splash_screen and removed redundant folders.

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,10 +1,34 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(seconds: 2),
+      () => Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const HomePage()),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const Scaffold(
+      body: Center(
+        child: Image(
+          image: AssetImage('assets/images/logo.jpg'),
+        ),
+      ),
+    );
   }
 }
