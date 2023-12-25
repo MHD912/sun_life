c7c01b3de54b9206c17139306b7c62a4719f4d22
Mohammad Hussein Hamed <hussein.912.workshop@gmail.com>
2023-12-25 15:07:45 +0300

    Updated .gitignore, replaced firebase with hive, deleted windows folder.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:sun_life/firebase_options.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:sun_life/models/person/person.dart';
 import 'views/splash_screen.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(PersonAdapter());
   runApp(const MainApp());
 }
 
